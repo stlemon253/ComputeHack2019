82179b0eebf56061aa6f0b63a365f5c67d45d300
stlemon253 <58526803+stlemon253@users.noreply.github.com>
2019-12-04 09:58:46 -0800

    Update get-app.sh

--- get-app.sh
+++ get-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 size=""
 ulimit -n 20000
-/usr/bin/wget -O /opt/server https://github.com/vmssignite/ignite2019/raw/controlcrash/server
+/usr/bin/wget -O /opt/server https://github.com/stlemon253/ComputeHack2019/blob/master/server
 sleep 5
 chmod +x /opt/server
 /opt/server
